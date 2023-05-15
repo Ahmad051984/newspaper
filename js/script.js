84834a336f9da39c066ef4153e683d06665af337
Ahmad051984 <ahmad1984@mail.ru>
2023-05-15 11:18:32 +0300

    Made changes to the burger menu

--- js/script.js
+++ js/script.js
@@ -3,4 +3,5 @@ const burger = document.querySelector('.header__burger');
 const menu = document.querySelector('.menu');
 burger.addEventListener("click", function(){
 	menu.classList.toggle('active')
+	burger.classList.toggle('active')
 })
